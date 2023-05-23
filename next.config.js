a6b65d3810a2b98c4c748766a376ec249d0050be
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2023-05-22 22:17:47 -0700

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  api: {
+    bodyParse: {
+      sizeLimit: "1mb", //set max size limit for the request body in API to prevent server from being overwhelmed by excessively large requests
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
